c02435350effb8439d5060340dafd8024f5f38ec
kapil-x-rijhwani <kapil.rijhwani@oracle.com>
2018-06-29 23:19:38 +0530

    1. credentials.py Adding credentials for Twitter App Access.

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys
+
+# Consume :
+CONSUMER_KEY = input('Enter your Consumer Key : ')
+CONSUMER_SECRET = input('Enter your Consumer Secret : ')
+
+# Access:
+ACCESS_TOKEN = input('Enter your Access Token : ')
+ACCESS_SECRET = input('Enter your Access Secret : ')
